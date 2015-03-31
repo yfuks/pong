c88682e0de60a8c9807c79d3202079bb9d7331c4
Sebastien PARIAUD <spariaud@e3r5p9.42.fr>
2015-03-31 17:30:31 +0200

    f

--- sources/ball.c
+++ sources/ball.c
@@ -6,7 +6,7 @@
 /*   By: spariaud <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/30 04:04:52 by spariaud          #+#    #+#             */
-/*   Updated: 2015/03/31 17:02:21 by spariaud         ###   ########.fr       */
+/*   Updated: 2015/03/31 17:29:57 by spariaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,51 @@
 
 static void physics_next(t_env *e)
 {
-	if (e->ball.new_center.x + 10 > (W_WIDTH - 10))
+	if (e->ball.new_center.x + 10 > (W_WIDTH - 15))
 	{
 		if (e->ball.new_center.y > e->coord_player_2.y &&
-			e->ball.new_center.y < e->coord_player_2.y + (W_HEIGHT / 10))
+			e->ball.new_center.y < e->coord_player_2.y + ((W_HEIGHT / 10) / 2))
 		{
 			e->ball.vitesse.x = -e->ball.vitesse.x;
-			e->ball.vitesse.y = rand() % 2;
-			if (rand() % 2 == 0)
-				e->ball.vitesse.y = -e->ball.vitesse.y;
+			e->ball.vitesse.y = -rand() % 6;
+		}
+		else if (e->ball.new_center.y > e->coord_player_2.y &&
+			e->ball.new_center.y < e->coord_player_2.y + ((W_HEIGHT / 10) / 2) * 2)
+		{
+			e->ball.vitesse.x = -e->ball.vitesse.x;
+			e->ball.vitesse.y = rand() % 6;
 		}
 		else
 		{
 			init_ball(e);
 			update_score_player_1(e);
+			return ;
 		}
-		return ;
 	}
 }
 
 void	physics(t_env *e)
 {
-	if (e->ball.new_center.x - 10 <= REC_WIDTH)
+	if (e->ball.new_center.x - 15 <= REC_WIDTH)
 	{
 		if (e->ball.new_center.y > e->coord_player_1.y &&
-			e->ball.new_center.y < e->coord_player_1.y + (W_HEIGHT / 10))
+			e->ball.new_center.y < e->coord_player_1.y + ((W_HEIGHT / 10) / 2))
 		{
 			e->ball.vitesse.x = -e->ball.vitesse.x;
-			e->ball.vitesse.y = rand() % 2;
-			if (rand() % 2 == 0)
-				e->ball.vitesse.y = -e->ball.vitesse.y;
+			e->ball.vitesse.y = -rand() % 6;
+		}
+		else if (e->ball.new_center.y > e->coord_player_1.y &&
+			e->ball.new_center.y < e->coord_player_1.y + ((W_HEIGHT / 10) / 2 * 2))
+		{
+			e->ball.vitesse.x = -e->ball.vitesse.x;
+			e->ball.vitesse.y = rand() % 6;
 		}
 		else
 		{
 			update_score_player_2(e);
 			init_ball(e);
+			return ;
 		}
-		return ;
 	}
 	physics_next(e);
 }
@@ -61,7 +69,7 @@ void	move_ball(t_env *e)
 	e->ball.old_center.y = e->ball.new_center.y;
 	e->ball.new_center.x += e->ball.vitesse.x;
 	e->ball.new_center.y += e->ball.vitesse.y;
-	if (e->ball.new_center.y - 10 <= 51)
+	if (e->ball.new_center.y - 5 <= 51)
 		e->ball.vitesse.y = -e->ball.vitesse.y;
 	if (e->ball.new_center.y + 10 >= W_HEIGHT)
 		e->ball.vitesse.y = -e->ball.vitesse.y;
@@ -73,8 +81,8 @@ void	init_ball(t_env *e)
 	e->ball.old_center.y = (W_HEIGHT / 2);
 	e->ball.new_center.x = e->ball.old_center.x;
 	e->ball.new_center.y = e->ball.old_center.y;
-	e->ball.vitesse.x = 1;
-	e->ball.vitesse.y = rand() % 2;
+	e->ball.vitesse.x = 4;
+	e->ball.vitesse.y = rand() % 6;
 	if (rand() % 2 == 0)
 	{
 		e->ball.vitesse.x = -e->ball.vitesse.x;
